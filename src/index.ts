2de3278d6f7cb5f633a92726cd9feea0d4f614d5
robertarnorsson <rha006@gmail.com>
2024-11-27 13:18:42 +0100

    Fixed CORS

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = new Hono<{ Bindings: Env }>()
 app.use(
   '*',
   cors({
-    origin: ['http://gzwmap.com', 'https://new.gzwmap.com', 'http://localhost:5173'],
+    origin: ['https://gzwmap.com', 'https://new.gzwmap.com', 'http://localhost:5173'],
     allowMethods: ['GET', 'OPTIONS'],
     allowHeaders: ['Content-Type']
   })
